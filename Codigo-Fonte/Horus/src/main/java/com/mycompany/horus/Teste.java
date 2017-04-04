d4bb74d5f62a1881671dde5bd5ef97265ac580f9
= <=>
2017-04-03 21:21:31 -0300

    Construção preliminar da arquitetura a ser utilizada

--- Codigo-Fonte/Horus/src/main/java/com/mycompany/horus/Teste.java
+++ Codigo-Fonte/Horus/src/main/java/com/mycompany/horus/Teste.java
@@ -1,4 +1,3 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,54 +5,88 @@
  */
 package com.mycompany.horus;
 
-import com.mycompany.horus.ws.EstoqueFault;
-import com.mycompany.horus.ws.EstoqueService;
-import com.mycompany.horus.ws.EstoqueServicePortType;
-import com.mycompany.horus.ws.MSFalha;
-import com.mycompany.horus.ws.RequestConsultarPosicaoEstoquePorCNES;
-import com.mycompany.horus.ws.ResponseConsultarPosicaoEstoquePorCNES;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.File;
+import java.io.FileOutputStream;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPConnection;
+import javax.xml.soap.SOAPConnectionFactory;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.soap.SOAPPart;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
 
 /**
  *
- * @author pedro
- */
-/**
- *
- * @author pedro
+ * @author aluno
  */
 public class Teste {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    private static SOAPMessage createSoapRequest() throws Exception {
 
-        EstoqueService service = new EstoqueService();
-        EstoqueServicePortType port = service.getEstoqueServicePort();
-        RequestConsultarPosicaoEstoquePorCNES requestConsultarPosicaoEstoquePorCNES = new RequestConsultarPosicaoEstoquePorCNES();
-        requestConsultarPosicaoEstoquePorCNES.setCnes(1234567);
+        MessageFactory messageFactory = MessageFactory.newInstance();
 
-        ResponseConsultarPosicaoEstoquePorCNES teste;
-        try {
-            teste = port.consultarPosicaoEstoquePorCNES(requestConsultarPosicaoEstoquePorCNES);
-            System.out.println(teste.toString());
-        } catch (EstoqueFault ex) {
-            MSFalha x = ex.getFaultInfo();
-            System.out.println();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+
+        SOAPEnvelope soapEnvelope = soapPart.getEnvelope();
+        soapEnvelope.addNamespaceDeclaration("end", "http://endpoint.concretepage.com/");
+
+        SOAPBody soapBody = soapEnvelope.getBody();
+
+        SOAPElement soapElement = soapBody.addChildElement("getWelcomeMsg", "end");
+        SOAPElement element1 = soapElement.addChildElement("arg0");
+        element1.addTextNode("EveryOne");
+
+        soapMessage.saveChanges();
+
+        System.out.println("----------SOAP Request------------");
+        soapMessage.writeTo(System.out);
+        return soapMessage;
+    }
+
+    private static void createSoapResponse(SOAPMessage soapResponse) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        Source sourceContent = soapResponse.getSOAPPart().getContent();
+        System.out.println("\n----------SOAP Response-----------");
+
+        File file = new File("horusExit.txt");
+        FileOutputStream fop = new FileOutputStream(file);
+
+        if (!file.exists()) {
+            file.createNewFile();
         }
 
-        //AuthSOAPHeader authSOAPHeader = new AuthSOAPHeader();
-        //authSOAPHeader.setUser("test");
-        //authSOAPHeader.setPassword("test");
-        //String payerList = port.ping(authSOAPHeader);
-        //Service service = Service.create(HorusFactory._Produto_QNAME);        
-        //Dispatch<Source> disp = service.createDispatch(
-        //        HorusFactory._Produto_QNAME, Source.class, Service.Mode.PAYLOAD
-        //);
-        //"a".toString();
-        //Source request = new StreamSource("<hello/>");
-        //Source response = disp.invoke(request);
+
+        StreamResult result = new StreamResult(file);
+        transformer.transform(sourceContent, result);
+        
+        fop.flush();
+        fop.close();
+    }
+
+    public static void main(String args[]) {
+        try {
+            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+            String url = "https://servicos.saude.gov.br/horus/v1r0/EstoqueService?WSDL";
+
+            SOAPMessage soapRequest = createSoapRequest();
+            SOAPMessage soapResponse = soapConnection.call(soapRequest, url);
+
+            createSoapResponse(soapResponse);
+            soapConnection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- README.md
+++ README.md
@@ -16,7 +16,6 @@ Lucas Nunes | ? | ?
 Guilherme Caexeta | ? | ?
 Valker Diniz | ? | ?
 
-
 ### 2.2. Cronogramas
 
 ### 2.3. Ferramentas Utilizadas
@@ -33,9 +32,13 @@ Link | Descrição
 [HORUS - Documentação](http://datasus.saude.gov.br/images/Interoperabilidade/Especificacao%20Tecnica%20para%20Integracao%20com%20o%20HORUS%20v1.pdf) | Documento .pdf contendo os passos para a integração, autenticação e consulta no Web Service do HORUS.
 [Barramento - SUS](https://github.com/kyriosdata/db/wiki/Barramento-SUS) | Repositório do Github contendo os sistemas presentes no DATASUS, incluindo o HORUS.
 [Exemplo wsimport](http://stackoverflow.com/questions/4172118/web-service-client-given-wsdl) | Exemplo funcional de um cliente em java para Web Service que contém autenticação.
+[Exemplo saaj](http://www.concretepage.com/webservices/java-saaj-web-service-example) | Exemplo do uso do Saaj para soap.
+[SAAJ](http://docs.oracle.com/javaee/5/tutorial/doc/bnbhr.html) | Documentação do Saaj pela Oracle
 
 ### 3.3. Solução Preliminar
-#### Qual a solução preliminar (dois parágrafos, no máximo) oferecida pela equipe? 
+#### Modelo arquitetural
+![Modelo prévio da arquitetura](./index/Modelagem De Dados.png)
+#### Detalhamento da arquitetura
 
 ## 4. Conclusão 
 Este sistema foi desenvolvido para a matéria de integração pelos alunos do 7º período da Universidade Federal de Goiás - UFG.
