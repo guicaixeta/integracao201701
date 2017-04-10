69f61c6ca3f10b9841b1c61b2372c26cd4b4f469
viniciuscmac <viniciuscmac@gmail.com>
2017-04-10 10:11:14 -0300

    Adicionando funcao de transformar em xml

--- Codigo-Fonte/Horus/src/main/java/com/mycompany/horus/Teste.java
+++ Codigo-Fonte/Horus/src/main/java/com/mycompany/horus/Teste.java
@@ -6,23 +6,42 @@
 package com.mycompany.horus;
 import javax.xml.soap.*;
 import javax.xml.namespace.QName;
+import java.io.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.xml.sax.InputSource;
+import org.w3c.dom.Document;
 
 public class Teste {
 
     public static void main(String args[]) throws Exception {
         // Creating SOAP Connection
-        SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
-        SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+        try {
+            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
 
-        // Sending SOAP Message to SOAP Server
-        String url = "https://servicos.saude.gov.br/horus/v1r0/EstoqueService?WSDL";
-        SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), url);
+            // Sending SOAP Message to SOAP Server
+            String url = "https://servicos.saude.gov.br/horus/v1r0/EstoqueService?WSDL";
+            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), url);
 
-        // print SOAP Response
-        System.out.println("Response SOAP Message:");
-        soapResponse.writeTo(System.out);
+            //Changing response to Xml file
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            soapResponse.writeTo(out);
+            String strMsg = new String(out.toByteArray());
+            stringToDom(strMsg);
+            
+            soapConnection.close();
+        }catch(Exception e){
+            System.err.println("Error occurred while sending SOAP Request to Server");
+            e.printStackTrace();
+        }
+    }
 
-        soapConnection.close();
+    public static void stringToDom(String xmlSource)
+            throws IOException {
+        java.io.FileWriter fw = new java.io.FileWriter("teste.xml");
+        fw.write(xmlSource);
+        fw.close();
     }
 
     private static SOAPMessage createSOAPRequest() throws Exception {
